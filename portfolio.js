c3882184c90992bc52f6a8c2ac1b0e2ba89ee54d
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-02-15 19:19:43 -0500

    hover effects added to projects on index.html, still broken with bottom margin

---
+++ portfolio.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+$('#portGall1').on('mouseenter', function() {
+  $('#modalContent1').show();
+});
+
+$('#portGall2').on('mouseenter', function() {
+  $('#modalContent2').show();
+});
+
+$('#portGall3').on('mouseenter', function() {
+  $('#modalContent3').show();
+});
+
+$('#portGall4').on('mouseenter', function() {
+  $('#modalContent4').show();
+});
+
+$('#portGall5').on('mouseenter', function() {
+  $('#modalContent5').show();
+});
+
+$('#portGall6').on('mouseenter', function() {
+  $('#modalContent6').show();
+});
+
+$('#portGall7').on('mouseenter', function() {
+  $('#modalContent7').show();
+});
+
+$('#portGall8').on('mouseenter', function() {
+  $('#modalContent8').show();
+});
+
+$('.modalContent').on('mouseleave', function() {
+  $(this).hide();
+});
+
+}); // end doc ready
